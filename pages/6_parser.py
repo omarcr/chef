68765d64e73e8d4353d6c20167016c015b5b147f
zamanova <zamanova@Susans-MacBook-Pro-2.local>
2025-01-26 15:36:42 -0500

    fix prompt

--- pages/6_parser.py
+++ pages/6_parser.py
@@ -3,30 +3,33 @@ from streamlit_pdf_viewer import pdf_viewer
 
 from llama_parse import LlamaParse
 import os
-os.environ["LLMA_CLOUD_API_KEY"] = "my_key"
-
-
-with open("assets/resume_Jennet_Zamanova.pdf", "rb") as file:
-    st.download_button(
-        label="Download resume as PDF",
-        data = file,
-        file_name="resume_Jennet_Zamanova.pdf",
-        mime="text/plain",
-    )
 
 
 file = st.file_uploader('Choose your .pdf file to upload', type="pdf")
 
 st.session_state.uploaded_file = file
 
+prompt = """Provide your answer in JSON structure like this: {
+    "[0-9]*": {
+        "dish_name": "The name of the dish",
+        "ingredients": "Ingredients of the dish",
+        "price": "Price of the dish",
+        "image": "Image of the dish"
+    }
+}.
+"""
+
 if file:
+    
     if st.button('Parse Uploaded pdf file (Powered by AI)'):
 
-        doc_parsed = LlamaParse(result_type="markdown",api_key="llx-vXZdRSAELbufIsFwWQClBgAXOWLzANydSev6sppcs2otLj9V", parsing_instruction="Extract the names of the dishes, their ingredients, prices, and the images if available."
+        
+        doc_parsed = LlamaParse(result_type="markdown",api_key= st.secrets["LLAMA_CLOUD_API_KEY"],
+                                complemental_formatting_instruction=prompt
                                 ).load_data(st.session_state.uploaded_file.getvalue(), extra_info={"file_name": "_"})
 
         st.write('checkpoint3')
-        st.write(doc_parsed)
+        # st.write(doc_parsed)
 
         st.write(doc_parsed[0].text)
 
